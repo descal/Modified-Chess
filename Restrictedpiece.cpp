b5ac05bd438c97d25c7fe6ac77d35b354acb864f
descal <migdc95@gmail.com>
2015-12-18 23:43:32 +0100

    Create Restrictedpiece.cpp

---
+++ Restrictedpiece.cpp
@@ -0,0 +1,30 @@
+#include "player.h"
+#include "restrictedpiece.h"
+
+RestrictedPiece::RestrictedPiece(bool isWhite) : Piece(isWhite)
+{
+    _moved = false;
+}
+
+RestrictedPiece::~RestrictedPiece()
+{
+}
+
+bool RestrictedPiece::moveTo(Player& byPlayer, Square& to)
+{
+    bool validMove = Piece::moveTo(byPlayer, to);
+
+    // only change _moved if a valid move is made
+    // and this piece hasn't already been moved
+    if(validMove && !_moved)
+    {
+        _moved = true;
+    }
+
+    return validMove;
+}
+
+bool RestrictedPiece::hasMoved() const
+{
+    return _moved;
+}
