9d50863b53543aa8f2d39fdd59a9d02d8439011d
descal <migdc95@gmail.com>
2015-12-18 23:30:54 +0100

    Create Restrictedpiece.h

---
+++ Restrictedpiece.h
@@ -0,0 +1,47 @@
+#ifndef RESTRICTED_H
+#define RESTRICTED_H
+
+#include "piece.h"
+
+class Player;
+
+using namespace std;
+
+class RestrictedPiece : public Piece
+{
+ public:
+
+    /**
+    * Creates a RestrictedPiece
+    *
+    * @param color the color of the piece
+    */
+    RestrictedPiece(bool isWhite);
+
+    /**
+     * Default destructor
+     */
+    virtual ~RestrictedPiece();
+
+    /**
+    * Move the piece to another square.
+    *
+    * @return successful move
+    */
+    virtual bool moveTo(Player& byPlayer, Square& to);
+
+    /**
+    * Get if the piece has been moved previously
+    *
+    * @return if moved
+    */
+    bool hasMoved() const;
+
+ private:
+
+    // private attributes
+    bool _moved;
+
+}; // RestrictedPiece
+
+#endif
